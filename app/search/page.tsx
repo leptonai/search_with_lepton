06d58d3bf70f812215524f9f4dc1ec28b8feda7a
Meng Zhang <meng@tabbyml.com>
2024-01-27 00:25:29 -0800

    update

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -9,7 +9,7 @@ export default function SearchPage() {
   const rid = decodeURIComponent(searchParams.get("rid") || "");
   return (
     <div className="absolute inset-0 bg-[url('/ui/bg.svg')]">
-      <div className="mx-auto max-w-3xl absolute inset-4 md:inset-8 bg-white">
+      <div className="mx-auto absolute inset-4 md:inset-8 bg-white">
         <div className="h-20 pointer-events-none rounded-t-2xl w-full backdrop-filter absolute top-0 bg-gradient-to-t from-transparent to-white [mask-image:linear-gradient(to_bottom,white,transparent)]"></div>
         <div className="px-4 md:px-8 pt-6 pb-24 rounded-2xl ring-8 ring-zinc-300/20 border border-zinc-200 h-full overflow-auto">
           <Title query={query}></Title>
