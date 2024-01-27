46dfc9e590ea5923d8a6b7f93176fc793cc9101b
Meng Zhang <meng@tabbyml.com>
2024-01-27 00:37:00 -0800

    update

--- app/utils/get-search-url.ts
+++ app/utils/get-search-url.ts
@@ -1,5 +1,3 @@
 export const getSearchUrl = (query: string, search_uuid: string) => {
-  const prefix =
-    process.env.NODE_ENV === "production" ? "/search.html" : "/search";
-  return `${prefix}?q=${encodeURIComponent(query)}&rid=${search_uuid}`;
+  return `/search?q=${encodeURIComponent(query)}&rid=${search_uuid}`;
 };
