28f9acc8ebba4098d921224ad51dacab5a3604e6
Meng Zhang <meng@tabbyml.com>
2024-01-27 17:46:32 -0800

    update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,10 @@
-import { Mails } from "lucide-react";
 import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
     <div className="text-center flex flex-col items-center text-xs text-zinc-700 gap-1">
       <div className="text-zinc-400">
-        Poor-man's oss perplexity.ai/phind implementation
+        Poor-man's oss perplexity.ai/phind.com implementation
       </div>
       <a target="_blank" href="https://github.com/wsxiaoys/bobtail.dev">Fork me on Github</a>
     </div>
