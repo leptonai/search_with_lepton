9232b5e1a9473a02299c63c211d730815ce40206
emrgnt-cmplxty <owen@algofi.org>
2024-01-28 14:48:57 -0800

    add speed insights

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      {/* <SpeedInsights /> */}
+      <SpeedInsights />
     </html>
   );
 }
