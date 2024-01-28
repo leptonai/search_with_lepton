af900c70c8d0aea70305fb0ab3390dd63a6d686a
emrgnt-cmplxty <owen@algofi.org>
2024-01-28 12:40:21 -0800

    add analytics

--- web/src/app/components/title.tsx
+++ web/src/app/components/title.tsx
@@ -15,6 +15,16 @@ export const Title = ({ query }: { query: string }) => {
         {query}
       </div>
       <div className="flex-none">
+      <button
+          onClick={() => {
+            router.push(getSearchUrl(encodeURIComponent(query), nanoid()));
+          }}
+          type="button"
+          className="rounded flex gap-2 items-center bg-transparent px-2 py-1 text-xs font-semibold text-blue-500 hover:bg-zinc-900"
+        >
+          <RefreshCcw size={12}></RefreshCcw>Rewrite
+        </button>
+
         <button
           onClick={() => {
             router.push(getSearchUrl(encodeURIComponent(query), nanoid()));
