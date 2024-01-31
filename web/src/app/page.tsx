9fdcdc3e9a16eb7e0d482687692ae4dd69c7a5a7
emrgnt-cmplxty <owen@algofi.org>
2024-01-30 19:58:41 -0800

    change query

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <Search></Search>
         <div className="flex gap-2 flex-wrap justify-center">
           <PresetQuery query="Who said live long and prosper?"></PresetQuery>
-          <PresetQuery query="Why do we only see one side of the moon?"></PresetQuery>
+          <PresetQuery query="What is the Higgs Boson and why should I care?"></PresetQuery>
         </div>
         <Footer></Footer>
       </div>
